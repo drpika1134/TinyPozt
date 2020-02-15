9e7e91d17429cb6758faee8d6107ea922cea551d
drpika1134 <drpika1134@gmail.com>
2020-02-15 15:49:27 -0600

    Added Auth mechanism

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import LoginForm from './components/Login/LoginForm'
+import PrivateRoute from './components/PrivateRoute'
+
+import checkTokenExist from './utils/checkTokenExist'
+
+checkTokenExist()
+
+function Test() {
+  useEffect(() => {
+    console.log('fetching data!')
+    async function fetchData() {
+      try {
+        const response = await axios.get('/api')
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [])
+  return <div>LMAo</div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>tes1t</h1>
-    </div>
+    <Router>
+      <Link to="/test">Test</Link>
+      <Switch>
+        <Route exact path="/">
+          <LoginForm />
+        </Route>
+        <PrivateRoute path="/test">
+          <Test />
+        </PrivateRoute>
+      </Switch>
+    </Router>
   )
 }
 
