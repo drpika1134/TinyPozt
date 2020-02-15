fffc4c1094d8f8fcafd61e9c41b23287cddc726e
drpika1134 <drpika1134@gmail.com>
2020-02-15 15:48:30 -0600

    Removed external connections

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,26 +1,27 @@
-/* PACKAGES */
 require('dotenv').config()
 const mongoose = require('mongoose')
-
-/* EXPRESS */
+const redis = require('redis')
 const express = require('express')
+const cookieParser = require('cookie-parser')
+
+const { connectRedis, connectMongoDB } = require('./connections')
+
 const PORT = process.env.PORT || 5000
+const redisPORT = process.env.REDIS_PORT || 6379
+
+// const client = connectRedis(redis, redisPORT)
+connectMongoDB(mongoose)
+
 const app = express()
 
 const postRoutes = require('./routes/post')
+const authRoutes = require('./routes/auth')
 
-/* MONGODB CONNECTION */
-mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@tinypozt-iujtx.mongodb.net/main?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
-const db = mongoose.connection
-db.on('error', () => console.log('error connecting to mongoDB'))
-db.once('open', () => console.log('connected to MongoDB'))
+app.use('/auth', authRoutes)
 
 app.use('/api', postRoutes)
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
