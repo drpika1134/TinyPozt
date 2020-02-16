0349cc82d7690c0f47e680bfd2ccbcca7ef58119
drpika1134 <drpika1134@gmail.com>
2020-02-16 13:04:17 -0600

    Updated types

--- app/frontend/src/redux/actions/types.js
+++ app/frontend/src/redux/actions/types.js
@@ -1,4 +1,10 @@
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_ERROR = 'LOGIN_ERROR'
 export const LOGOUT = 'LOGOUT'
 export const SET_AUTH = 'SET_AUTH'
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_PROGRESS = 'LOGIN_PROGRESS'
+export const LOGIN_ERROR = 'LOGIN_ERROR'
+
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
+export const REGISTER_PROGRESS = 'REGISTER_PROGRESS'
+export const REGISTER_ERROR = 'REGISTER_ERROR'
